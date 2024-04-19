1d1a62a5c40799706004ec3003abbea35537b959
lorenztheg <114148980+lorenztheg@users.noreply.github.com>
2024-04-19 18:05:25 +0200

    cleanup

--- src/main/java/at/ac/fhcampuswien/fhmdb/FhmdbApplication.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/FhmdbApplication.java
@@ -1,20 +1,11 @@
 package at.ac.fhcampuswien.fhmdb;
 
-import at.ac.fhcampuswien.fhmdb.api.MovieAPI;
-import at.ac.fhcampuswien.fhmdb.models.Movie;
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.util.Objects;
-
-import static at.ac.fhcampuswien.fhmdb.HomeController.descriptionsList;
-import static at.ac.fhcampuswien.fhmdb.HomeController.titlesList;
 
 public class FhmdbApplication extends Application {
     @Override
@@ -26,20 +17,6 @@ public class FhmdbApplication extends Application {
         stage.setScene(scene);
         stage.show();
 
-        ObservableValue<Number> sceneWidth = stage.widthProperty();
-
-        sceneWidth.addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                double newWidth = newValue.doubleValue() - 50;
-                for (Label title : titlesList) {
-                    title.setMaxWidth(newWidth);
-                }
-                for (Label detail : descriptionsList) {
-                    detail.setMaxWidth(newWidth);
-                }
-            }
-        });
     }
 
     public static void main(String[] args) {
